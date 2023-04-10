4048307d2b0df08b5a4c72af6a81eeee6d21962d
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2023-04-10 05:47:09 -0700

    lang fr

---
+++ lang/fr/auth.php
@@ -0,0 +1,67 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'failed'   => 'Ces identifiants ne correspondent pas à nos enregistrements.',
+    'throttle' => 'Trop de tentatives de connexion. Veuillez réessayer dans :seconds secondes.',
+
+    'full_name'        => 'Nom complet',
+    'email'            => 'E-mail',
+    'password'         => 'Mot de passe',
+    'confirm_password' => 'Confirmez le mot de passe',
+    'remember_me'      => 'Se souvenir de moi',
+    'sign_in'          => 'Se connecter',
+    'sign_out'         => 'Se déconnecter',
+    'register'         => "S'inscrire",
+
+    'login' => [
+        'title'               => 'Se connecter',
+        'forgot_password'     => 'J\'ai oublié mon mot de passe',
+        'register_membership' => 'S\'inscrire pour une nouvelle adhésion',
+    ],
+
+    'registration' => [
+        'title'           => "S'inscrire pour une nouvelle adhésion",
+        'i_agree'         => "Je suis d'accord avec les",
+        'terms'           => 'termes et conditions',
+        'have_membership' => "J'ai déjà une adhésion",
+    ],
+
+    'forgot_password' => [
+        'title'          => 'Entrez votre adresse e-mail pour réinitialiser le mot de passe',
+        'send_pwd_reset' => 'Envoyer le lien de réinitialisation du mot de passe',
+    ],
+
+    'reset_password' => [
+        'title'         => 'Réinitialiser votre mot de passe',
+        'reset_pwd_btn' => 'Réinitialiser le mot de passe',
+    ],
+
+    'confirm_passwords' => [
+        'title'                => 'Veuillez confirmer votre mot de passe avant de continuer.',
+        'forgot_your_password' => 'Vous avez oublié votre mot de passe ?',
+    ],
+
+    'verify_email' => [
+        'title'       => 'Vérifiez votre adresse e-mail',
+        'success'     => 'Un nouveau lien de vérification a été envoyé à votre adresse e-mail.',
+        'notice'      => "Avant de continuer, veuillez vérifier votre e-mail pour un lien de vérification. Si vous n'avez pas reçu l'e-mail,",
+        'another_req' => 'Cliquez ici pour en faire une autre demande',
+    ],
+
+    'emails' => [
+        'password' => [
+            'reset_link' => 'Cliquez ici pour réinitialiser votre mot de passe',
+        ],
+    ],
+    ];
---
+++ lang/fr/models/couvertureMedicals.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'singular' => 'Couverture Médicale',
+    'plural' => 'Couvertures Médicales',
+    'fields' => [
+    'id' => 'Id',
+    'nom' => 'Nom',
+    'description' => 'Description',
+    'created_at' => 'Créé le',
+    'updated_at' => 'Mis à jour le',
+    ],
+];
