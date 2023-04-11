b1b9be917e458e3bfff1976b6583beacc017975c
lharrak-botaina <lharrak.botaina@gmail.com>
2023-04-11 13:58:35 +0000

    [ADD]patients migration

--- resources/views/services/create.blade.php
+++ resources/views/services/create.blade.php
@@ -1,9 +1,9 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<!-- Summernote CSS - CDN Link -->
+<link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css" rel="stylesheet">
+<!-- //Summernote CSS - CDN Link -->
+{{-- link add --}}
+<link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.2/summernote.css" rel="stylesheet">
 
-  <!-- Summernote CSS - CDN Link -->
-  <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
-  <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css" rel="stylesheet">
-  <!-- //Summernote CSS - CDN Link -->
 @extends('layouts.app')
 
 @section('content')
@@ -11,9 +11,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-12">
-                    <h1>
-                        @lang('crud.create') @lang('models/services.singular')
-                    </h1>
+
                 </div>
             </div>
         </div>
@@ -28,9 +26,9 @@
                     <div class="col-md-12  ">
                         <div class="card card-primary card-create ">
                             <div class="card-header">
-                                <h3 class="card-title">Ajouter un nouveau service</h3>
+                                <h3 class="card-title">@lang('crud.create') @lang('models/services.singular')</h3>
                             </div>
-                            <div class="card-body">
+                            <div class="card-body ">
                                 {!! Form::open(['route' => 'services.store']) !!}
                                 <!-- Date -->
                                 <div class="row">
@@ -39,11 +37,19 @@
 
 
                             </div>
-                            <div class="card-footer d-flex justify-content-between">
-                                {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
-                                <a href="{{ route('services.index') }}" class="btn btn-secondary"> @lang('crud.cancel') </a>
-                            </div>
 
+                            <div class="card-footer">
+                                <div class="d-flex bd-highlight mb-3">
+                                    <div class="p-2 bd-highlight">
+                                        {!! Form::submit(__('crud.save'), ['class' => 'btn btn-primary']) !!}
+
+                                    </div>
+                                    <div class="ml-auto p-2 bd-highlight">
+                                        <a href="{{ route('services.index') }}" class="btn btn-secondary"> @lang('crud.cancel') </a>
+                                    </div>
+
+                                </div>
+                            </div>
                             {!! Form::close() !!}
                         </div>
                     </div>
@@ -54,44 +60,24 @@
         </div>
         <!-- /.container-fluid -->
     </section>
-    {{-- <div class="content px-3">
 
-        @include('adminlte-templates::common.errors')
+    {{-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 
-        <div class="card ">
+    <!-- Summernote JS - CDN Link -->
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
+    {{-- link add --}}
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js"
+        integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote.js" defer></script>
+    <script>
+        $(document).ready(function() {
+            $('#description').summernote({
+                placeholder: 'Saisir une description du service.',
 
-            {!! Form::open(['route' => 'services.store']) !!}
-
-            <div class="card-body">
-
-                <div class="row">
-                    @include('services.fields')
-                </div>
-
-            </div>
-
-            <div class="card-footer">
-                {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
-                <a href="{{ route('services.index') }}" class="btn btn-default"> @lang('crud.cancel') </a>
-            </div>
-
-            {!! Form::close() !!}
-
-        </div>
-    </div> --}}
+                height: 100,
+            });
+            $('.dropdown-toggle').dropdown();
+        });
+    </script>
 @endsection
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-
-<!-- Summernote JS - CDN Link -->
-<script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
-<script>
-  $(document).ready(function() {
-    $('#description').summernote({
-      placeholder: 'Saisir une description du service.',
-
-      height: 100
-    });
-    $('.dropdown-toggle').dropdown();
-  });
-</script>
