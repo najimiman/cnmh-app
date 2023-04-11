06511ef7719e1c90dd04b5d82b2b13edd52fed3e
najimiman <114479872+najimiman@users.noreply.github.com>
2023-04-11 13:27:04 +0000

    type dendicap

---
+++ database/migrations/2023_04_11_132239_create_reclamations_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reclamations', function (Blueprint $table) {
+            $table->id();
+            $table->string('Objet');
+            $table->string('Description');
+            $table->string('Remarques');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reclamations');
+    }
+};
