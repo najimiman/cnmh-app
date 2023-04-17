96d34119dbb05202e6e1b5e5decde3eefbb16241
Mohamed yahya <92155140+Mohamed-Yahyaa@users.noreply.github.com>
2023-04-17 15:01:44 +0000

    update changes

--- resources/views/niveau_scolaires/index.blade.php
+++ resources/views/niveau_scolaires/index.blade.php
@@ -7,7 +7,11 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>@lang('models/niveauScolaires.plural')</h1>
+                    <h1>
+                        @lang('models/niveauScolaires.singular')
+                        {{strtolower(__('models/niveauScolaires.plural'))}}</h1>
+                    
+                        
                 </div>
                 <div class="col-sm-6">
                     <a class="btn btn-primary float-right"
