f9c33dc44a3177b5e7027fa2c935cbe7f23a44dc
bilalbarina <dev.barina@gmail.com>
2023-04-19 14:35:10 +0000

    Updated footer

--- resources/views/root/layouts/app.blade.php
+++ resources/views/root/layouts/app.blade.php
@@ -58,10 +58,8 @@
 
         <!-- Main Footer -->
         <footer class="main-footer">
-            <div class="float-right d-none d-sm-block">
-                <b>Version</b> 3.1.0
-            </div>
-            <strong>Copyright &copy; 2014-2023 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights
+            <strong>
+                Copyright &copy; 2023 CNMH.</strong> All rights
             reserved.
         </footer>
     </div>
