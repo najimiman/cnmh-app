c201202b54b262226549a69a19f0f0d095a5bd8e
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2023-04-27 08:13:24 -0700

    modification employe

--- app/app/Imports/ImportEmployes.php
+++ app/app/Imports/ImportEmployes.php
@@ -3,9 +3,11 @@
 namespace App\Imports;
 
 use App\Models\Employe;
+use App\Models\Fonction;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithStartRow;
 
-class ImportEmployes implements ToModel
+class ImportEmployes implements ToModel,WithStartRow
 {
     /**
     * @param array $row
@@ -16,6 +18,7 @@ class ImportEmployes implements ToModel
     {
         $date_naissance = date('Y-m-d', strtotime($row[5]));
         $date_embauche = date('Y-m-d', strtotime($row[7]));
+        $fonction = Fonction::where('nom',$row[8])->get();
         return new Employe([
             'nom' => $row[0],
             'prenom' => $row[1],
@@ -25,7 +28,11 @@ class ImportEmployes implements ToModel
             'date_naissance' => $date_naissance,
             'cin' => $row[6],
             'date_embauche' => $date_embauche,
-            'fonction_id' => [""],
+            'fonction_id' => $fonction[0]->id,
         ]);
+    }
+    public function startRow(): int
+    {
+        return 2; // Start importing from row 2, skipping the first row
     }
 }
